ff514118cc847a5f7b00d56ba6967d43f38236ee
Liegeblade <emorelos04@gmail.com>
2025-04-25 13:35:21 -0700

    added white flag ship type

--- src/Ship.js
+++ src/Ship.js
@@ -2,44 +2,62 @@ class Ship extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y) {
         super(scene, x, y);
         this.scene = scene;
-        this.setOrigin(0.5, 0.5);
         this.setScale(0.6);
         this.setAngle(90);
-        this.health = 15;
+        this.health, this.points, this.speed;
     }
 
     update() {
         this.x -= 2;
     }
 
+    // fires 3 shots, curved path
     static RedCross = class extends Ship {
         constructor(scene, x, y) {
             super(scene, x, y);
-            this.setTexture("pirateMisc", "ship (1).png");
+            this.health = 15;
+            this.points = 3;
+            this.speed = 2;
         }
     }
 
+    // fires heavy shot, straight path
     static GreenSwords = class extends Ship {
         constructor(scene, x, y) {
             super(scene, x, y);
-            this.setTexture("pirateMisc", "ship (2).png");
             this.health = 3;
+            this.points = 1;
+            this.speed = 2;
         }
     }
 
+    // fires sideways, faster, straight path
     static BlueCavalier = class extends Ship {
         constructor(scene, x, y) {
             super(scene, x, y);
-            this.setTexture("pirateMisc", "ship (3).png");
             this.health = 3;
+            this.points = 1;
+            this.speed = 4;
         }
     }
 
+    // fires single shot, path is sine wave
     static YellowMark = class extends Ship {
         constructor(scene, x, y) {
             super(scene, x, y);
-            this.setTexture("pirateMisc", "ship (4).png");
             this.health = 9;
+            this.points = 2;
+            this.speed = 2;
+        }
+    }
+
+    // unarmed ship, nonmoving target
+    static WhiteFlag = class extends Ship {
+        constructor(scene, x, y) {
+            super(scene, x, y);
+            this.health = 3;
+            this.points = 1;
+            this.speed = 1;
         }
     }
 }
