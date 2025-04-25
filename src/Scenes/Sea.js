d9b371da763cb8bb9d1a7770bfe740bd7b52fee5
Ethan Morelos <etmorelo@ucsc.edu>
2025-04-25 01:14:15 -0700

    added cannonball + sound on click

--- src/Scenes/Sea.js
+++ src/Scenes/Sea.js
@@ -3,7 +3,7 @@ class Sea extends Phaser.Scene {
         super("sea"); // super({ key: 'Sea' });
         this.my = {sprite: {}};
 
-        this.originX = game.config.width / 12;
+        this.originX = 100;
         this.originY = game.config.height / 2;
     }
 
@@ -13,12 +13,26 @@ class Sea extends Phaser.Scene {
         this.load.atlasXML("pirateMisc", "piratePack_ships_spritesheet.png", "piratePack_ships_spritesheet.xml");
         this.load.atlasXML("uiAdventure", "ui_adventure_spritesheet", "ui_adventure_spritesheet.xml");
         // this.load.image("pirateTiles", "piratePack_tilesheet"); // tiles
+
+        this.load.audio("cannonFire", "cannonFire.mp3");
+        this.load.audio("woodSmash", "woodSmash.mp3");
+        this.load.audio("waterRush", "waterRush_0.mp3");
+        this.load.audio("windAmbience", "windAmbience_0.mp3");
     }
 
     create(){
         let my = this.my;
 
         my.sprite.pirateShip = this.add.sprite(this.originX, this.originY, "pirateMisc", "ship (2).png").setScale(0.5).setAngle(270);
+
+        this.pointer = this.input.activePointer;
+
+        this.input.on('pointerdown', (pointer) => {
+            if(pointer.leftButtonDown()) {
+                my.sprite.cannonball = this.add.sprite(my.sprite.pirateShip.x + 50, my.sprite.pirateShip.y, "pirateMisc", "cannonBall.png");
+                this.sound.play("cannonFire", {volume: 0.5});
+            }
+        });
 
         this.keyUp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
         this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
