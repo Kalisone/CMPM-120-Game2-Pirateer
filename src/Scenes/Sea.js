609b574cc3a72f9eb7d862f84958d52debb7c8fe
Ethan Morelos <etmorelo@ucsc.edu>
2025-05-04 21:41:48 -0700

    added smoke animation

--- src/Scenes/Sea.js
+++ src/Scenes/Sea.js
@@ -1,29 +1,42 @@
 class Sea extends Phaser.Scene {
     constructor() {
         super("sea"); // super({ key: 'Sea' });
-        this.my = {sprite: {}};
-        my.enemies = [];
+        this.my = {sprite: {}, text: {}};
+
+        this.my.sprite.shots = [];
+        this.maxShots = 12, this.reload = 36, this.reloadCounter = 0;
+
+        this.my.sprite.enemies = [];
         this.maxEnemies = 12;
 
         this.playerX_OG = 100;
         this.playerY_OG = game.config.height / 2;
+
+        this.score = 0;
     }
 
     preload() {
         this.load.setPath("./assets/");
 
+        // Sprites
         this.load.atlasXML("pirateMisc", "piratePack_ships_spritesheet.png", "piratePack_ships_spritesheet.xml");
         this.load.atlasXML("uiAdventure", "ui_adventure_spritesheet.png", "ui_adventure_spritesheet.xml");
         this.load.atlasXML("tanks", "tanks_spritesheet.png", "tanks_spritesheet.xml");
         // this.load.image("pirateTiles", "piratePack_tilesheet"); // tiles
 
+        // Sound Effects
         this.load.audio("cannonFire", "cannonFire.mp3");
         this.load.audio("woodSmash_0", "woodSmash_0.mp3");
         this.load.audio("woodSmash_1", "woodSmash_1.mp3");
         this.load.audio("woodSmash_2", "woodSmash_2.mp3");
         this.load.audio("waterRush", "waterRush_0.mp3");
         this.load.audio("windAmbience", "windAmbience_0.mp3");
+
+        // Music
         this.load.audio("PirateCrew", "PirateCrew_RossBugden.mp3");
+
+        // Font
+        //this.load.bitmapFont();
     }
 
     create(){
@@ -37,26 +50,8 @@ class Sea extends Phaser.Scene {
         //music.play({loop: true, volume: 0.5});
 
         // Sea scene variables
-        this.pirateSpeed = 6, this.shotSpeed = 12;
-
         let my = this.my;
-        this.maxShots = 6, this.reload = 36, this.reloadCounter = 0;
         my.sprite.cannonShots = [], my.sprite.cannonSmoke = [];
-        
-        // Pirate Ship controls
-        let keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-        let keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-        this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-
-        // Add Pirate Ship
-        //my.sprite.pirateShip = this.add.sprite(this.playerX_OG, this.playerY_OG, "pirateMisc", "ship (2).png").setScale(0.6).setAngle(270);
-        my.sprite.pirateShip = new PlayerShip(this, this.playerX_OG, this.playerY_OG, keyW, keyS).setScale(0.6).setAngle(270);
-
-        // Pirate Ship Cannon Shots
-        for(let i = 0; i < this.maxShots; i++){
-            my.sprite.cannonShots.push(this.add.sprite(-100, -100, "pirateMisc", "cannonBall.png"));
-            my.sprite.cannonShots[i].visible = false;
-        }
         
         // Pirate Ship Cannon Smoke
         my.sprite.cannonSmoke.push(this.add.sprite(-100, -100, "tanks", "smokeWhite0.png").setScale(0.4).setAlpha(0.9));
@@ -67,6 +62,34 @@ class Sea extends Phaser.Scene {
 
         my.sprite.cannonSmoke.push(this.add.sprite(-100, -100, "tanks", "smokeWhite5.png").setScale(0.3).setAlpha(0.5));
         my.sprite.cannonSmoke[2].visible = false;
+
+        this.anims.create({
+            key: "cannonSmoke",
+            frames: [{
+                key: "tanks",
+                frames: [{
+                            key: "smokeWhite0.png",
+                            key: "smokeWhite4.png",
+                            key: "smokeWhite5.png"
+                        }
+                    ]
+                }
+            ]
+
+        });
+
+        // PLAYER
+        let keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        let keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+        my.sprite.pirateShip = new PlayerShip(this, this.playerX_OG, this.playerY_OG, keyW, keyS).setScale(0.6).setAngle(270);
+
+        // Pirate Ship Cannon Shots
+        for(let i = 0; i < this.maxShots; i++){
+            my.sprite.cannonShots.push(this.add.sprite(-100, -100, "pirateMisc", "cannonBall.png"));
+            my.sprite.cannonShots[i].visible = false;
+        }
 
         // New enemy for testing
         my.sprite.enemyShip = new EnemyShip(this, 200, 200, 6).setScale(0.6).setAngle(90);
